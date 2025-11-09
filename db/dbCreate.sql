6c8640730252abc6d1dc4fea8f4ef633d6c02978
Vrasmos <vramos120304@gmail.com>
2025-11-09 19:03:59 +0100

    Completed dbCreate

--- db/dbCreate.sql
+++ db/dbCreate.sql
@@ -4,13 +4,62 @@ CREATE DATABASE UniRed;
 
 USE UniRed;
 
-CREATE TABLE IF NOT EXISTS USER (
-    userId CHAR(20) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS USERS (
+    userId INT AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL UNIQUE,
     email CHAR(63) NOT NULL UNIQUE,
     password CHAR(128) NOT NULL,
-    phoneNumber CHAR(31) NULL,
     name CHAR(31) NULL, 
-    birthDate date NULL,
-    typology BOOLEAN NOT NULL DEFAULT FALSE,
+    birthdate DATE NULL,
+    avatar BLOB,
+    typology ENUM('admin', 'utente') NOT NULL DEFAULT 'utente',
     primary key (userId)
+);
+
+CREATE TABLE IF NOT EXISTS GROUPS (
+    groupId INT AUTO_INCREMENT,
+    name CHAR(20) NOT NULL UNIQUE,
+    longdescription VARCHAR(128) NOT NULL,
+    avatar BLOB,
+    primary key (groupId)
+);
+
+CREATE TABLE IF NOT EXISTS POSTS (
+    postId INT AUTO_INCREMENT,
+    title VARCHAR(128) NOT NULL,
+    longdescription TEXT NOT NULL,
+    upvote INT DEFAULT 0,
+    downvote INT DEFAULT 0,
+    groupId INT,
+    userId INT,
+    primary key (postId),
+    FOREIGN KEY (groupId) REFERENCES GROUPS(groupId) ON DELETE CASCADE,
+    FOREIGN KEY (userid) REFERENCES USERS(userid) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS COMMENTS (
+    commentId INT AUTO_INCREMENT,
+    longdescription VARCHAR(100) NOT NULL,
+    userId INT,
+    postId INT,
+    primary key (commentId),
+    FOREIGN KEY (userId) REFERENCES USERS(userId) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (postId) REFERENCES POSTS(postId)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS PARTICIPANT (
+    userId INT,
+    groupId INT,
+    subscription_date DATE,
+    primary key (userId, groupId),
+    FOREIGN KEY (userId) REFERENCES USERS(userId)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (groupId) REFERENCES GROUPS(groupId)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
 );
